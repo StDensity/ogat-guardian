a1ada399d1f5162fe6dc3fd13facf611c649af53
StDensity <noyaljosephnnj@gmail.com>
2024-12-06 07:36:21 +0530

    Implemented footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='container mx-auto'>Footer</footer>
-  )
-}
+    <footer className="">
+      <div className="h-4 w-full bg-customDarkBlue" />
+      <div className="w-full bg-guardianBlue pb-10 pt-4">
+        <div className="container mx-auto flex justify-center gap-24 font-noto_serif text-white">
+          <div>
+            <Link href={"/front-page"}>
+              <h1 className="text-3xl font-bold md:text-5xl lg:text-6xl">
+                THE GUARDIAN
+              </h1>
+              <p className="text-2xl">OGAT NEWS SERVICE</p>
+            </Link>
+          </div>
 
-export default Footer
+          <div className="font-open_sans">
+            <Link className="font-bold hover:underline" href={"/about-us"}>
+              About us
+            </Link>
+            <address>
+              The Guardian News ltd. <br />
+              Medic Street 14 1000 <br />
+              River City
+            </address>
+          </div>
+
+          <div className="flex flex-col">
+            <Link className="font-bold hover:underline" href={"/about-us"}>
+              Terms of Service
+            </Link>
+            <Link className="font-bold hover:underline" href={"/about-us"}>
+              Subscription
+            </Link>
+            <Link className="font-bold hover:underline" href={"/about-us"}>
+              Contact Us
+            </Link>
+            <Link className="font-bold hover:underline" href={"/about-us"}>
+              Digital Archive
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
