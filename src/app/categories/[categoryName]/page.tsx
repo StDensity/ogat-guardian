6716e3b49db8d5f07718c28120fd10fed217093d
StDensity <noyaljosephnnj@gmail.com>
2024-12-12 00:43:31 +0530

    Added metadata and opengraph for category page

--- src/app/categories/[categoryName]/page.tsx
+++ src/app/categories/[categoryName]/page.tsx
@@ -1,8 +1,39 @@
-import { getAllNormalNews, getAllSportNews } from "@/app/lib/dataFetcher";
+import {
+  getAllNewsCategories,
+  getAllNormalNews,
+  getAllSportNews,
+} from "@/app/lib/dataFetcher";
 import React from "react";
 import FirstRow from "./FirstRow";
 import SecondRow from "./SecondRow";
+import { Metadata } from "next";
 
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ categoryName: string }>;
+}): Promise<Metadata> => {
+  const { categoryName } = await params;
+  const categoryData = await getAllNewsCategories();
+  const currentCategory = categoryData.find(
+    (category) => category.fields.slug == categoryName,
+  );
+  return {
+    title: `${currentCategory?.fields.name}`,
+    description: "Your page description",
+    openGraph: {
+      title: `Category - ${currentCategory?.fields.name}`,
+      description: "OGAT News Service Sports Category",
+      images: [
+        {
+          url: "/ogat_guardian_img.png",
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+};
 const CategoryPage = async ({
   params,
 }: {
